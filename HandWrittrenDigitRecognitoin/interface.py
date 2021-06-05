0c84adc3888e6f3070a3b10326017794f3fd22e4
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-06-06 01:27:52 +0500

    making interface to deploy module

---
+++ HandWrittrenDigitRecognitoin/interface.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+root.title( 'Handwritten Digit Recognition Module' )  # title
+root.configure( bg = '#bcdebb' )
+root.geometry( "900x600+300+50" )  # (width X hight + from_right + from_left))
+root.minsize( 400 , 200 )
+root.resizable(0,0)
+
+root.mainloop()
