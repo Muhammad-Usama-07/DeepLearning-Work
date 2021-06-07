77958e999881ccb01118dd33ce9bdd19ccf2fa3e
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-06-07 20:24:26 +0500

    *Making predict method
    A method that capable of take image form canvas and predict from machine learning model

--- HandWrittrenDigitRecognitoin/ProjectWithTkinterGUI/interface.py
+++ HandWrittrenDigitRecognitoin/ProjectWithTkinterGUI/interface.py
@@ -1,41 +1,49 @@
 from tkinter import *
+from tkinter import colorchooser, filedialog, messagebox
+from tkinter.filedialog import asksaveasfilename
 
+import PIL.ImageGrab as ImageGrab
 
 window = Tk()
 
+
 def draw(event):
-    x1,y1 = (event.x-2), (event.y-2)
-    x2,y2 = (event.x+2), (event.y+2)
-    canvas.create_oval(x1,y1,x2,y2, fill='white', outline = 'white', width = pen_size)
+    x1, y1 = (event.x - 2), (event.y - 2)
+    x2, y2 = (event.x + 2), (event.y + 2)
+    canvas.create_oval(x1, y1, x2, y2, fill='white', outline='white', width=pen_size)
+
 
 def clear():
     canvas.delete("all")
 
 
+def predict():
+    pass
+
 window.title('Paint')
-window.configure( bg = '#0C85DC' )
-window.geometry( "900x600+300+50" )
+window.configure(bg='#0C85DC')
+window.geometry("900x600+300+50")
 window.rowconfigure(0, weight=1)
 window.columnconfigure(0, weight=1)
 menubar = Menu(window)
 
-Label( window , text = " Handwritten Digit Recognition " , bg = "#0065AE" , relief = "solid" ,
-                height = 2 , font = "Times%New%Roman 18 bold italic" , anchor=CENTER).pack(side = TOP,fill=BOTH)
+Label(window, text=" Handwritten Digit Recognition ", bg="#0065AE", relief="solid",
+      height=2, font="Times%New%Roman 18 bold italic", anchor=CENTER).pack(side=TOP, fill=BOTH)
 
-canvas = Canvas(window, width=300,height=300, background='black')
-canvas.place(relx=0.2, rely=0.4,anchor=CENTER)
+canvas = Canvas(window, width=300, height=300, background='black')
+canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 canvas.bind('<B1-Motion>', draw)
 
 pen_size = 20
 
-predict_button = Button(window, text = "Predict" , bg = '#0065AE' , width = 8,
-                               font = ("Times%New%Roman" , 15 , "bold") ,
-                               relief = "ridge" )
-predict_button.place(relx=0.1, rely=0.7,anchor=CENTER)
-clear_button = Button(window, text='Clear', bg = '#0065AE' , width = 8,
-                               font = ("Times%New%Roman" , 15 , "bold") ,
-                               relief = "ridge", command= clear)
-clear_button.place(relx=0.3, rely=0.7,anchor=CENTER)
+predict_button = Button(window, text="Predict", bg='#0065AE', width=8,
+                        font=("Times%New%Roman", 15, "bold"),
+                        relief="ridge", command = predict)
+predict_button.place(relx=0.4, rely=0.8, anchor=CENTER)
+clear_button = Button(window, text='Clear', bg='#0065AE', width=8,
+                      font=("Times%New%Roman", 15, "bold"),
+                      relief="ridge", command=clear)
+clear_button.place(relx=0.6, rely=0.8, anchor=CENTER)
 
 window.mainloop()
