06e64dc9f5210920515555803a1b6be22a8cab8f
Muhammad Usama <muhammadusama2k17@gmail.com>
2021-06-30 06:50:51 -0700

    changing background color

--- Cat_and_Dog_classification/CompleteProject/interface.py
+++ Cat_and_Dog_classification/CompleteProject/interface.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.ttk import *
@@ -11,11 +12,13 @@ import os
 window = Tk()
 
 window.title('Cat and Dog Classification')
-window.configure(bg='#0C85DC')
+window.configure(bg='#ED9850')
 window.geometry("900x600+300+50")
 window.rowconfigure(0, weight=1)
 window.columnconfigure(0, weight=1)
 
-
+lb1 = tkinter.Label(text="Cat and Dog Classification", bg="#DA902D", relief="solid",
+      height=2, font="Times%New%Roman 18 bold italic", anchor=CENTER)
+lb1.pack(side=TOP, fill=BOTH)
 
 window.mainloop()
