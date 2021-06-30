3abf899e8ad4a03cd5427398b460037b65a9de7b
Muhammad Usama <muhammadusama2k17@gmail.com>
2021-06-30 06:44:51 -0700

    making window

--- Cat_and_Dog_classification/CompleteProject/interface.py
+++ Cat_and_Dog_classification/CompleteProject/interface.py
@@ -4,9 +4,18 @@ from tkinter.ttk import *
 import time
 from tkinter import colorchooser, filedialog, messagebox
 from tkinter.filedialog import asksaveasfilename
-from tensorflow.keras import models
-from tensorflow.keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import load_img
-from functools import partial
 import PIL.ImageGrab as ImageGrab
 import os
+
+
+window = Tk()
+
+window.title('Cat and Dog Classification')
+window.configure(bg='#0C85DC')
+window.geometry("900x600+300+50")
+window.rowconfigure(0, weight=1)
+window.columnconfigure(0, weight=1)
+
+
+
+window.mainloop()
