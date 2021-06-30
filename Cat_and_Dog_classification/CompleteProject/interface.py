247039cdca92341efe68d4f24a1cce41865efb1b
Muhammad Usama <muhammadusama2k17@gmail.com>
2021-06-30 11:45:54 -0700

    formatting code

--- Cat_and_Dog_classification/CompleteProject/interface.py
+++ Cat_and_Dog_classification/CompleteProject/interface.py
@@ -16,11 +16,8 @@ def upload():
     global image_path
     filename = fd.askopenfilename()
     image_path = filename
-
-
-def predict(path):
     # Getting image
-    my_pic = Image.open(path)
+    my_pic = Image.open(image_path)
     # Resizing Image
     resized = my_pic.resize((150, 150), Image.ANTIALIAS)
     # Displaying image
@@ -29,10 +26,13 @@ def predict(path):
     img_label.image = new_pic
     img_label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
+def predict(path):
+    pass
+
 
 window.title('Cat and Dog Classification')
 window.configure(bg='#ED9850')
-window.geometry("500x600+300+50")
+window.geometry("500x500+500+100")
 window.rowconfigure(0, weight=1)
 window.columnconfigure(0, weight=1)
 
@@ -46,10 +46,10 @@ lb2.place(relx=0.5, rely=0.2, anchor=CENTER)
 predict_button = tkinter.Button(text="Predict", bg='#DA902D', width=7,
                                 font=("Times%New%Roman", 15, "bold"),
                                 relief="solid", command=lambda: predict(image_path))
-predict_button.place(relx=0.4, rely=0.8, anchor=CENTER)
+predict_button.place(relx=0.4, rely=0.6, anchor=CENTER)
 upload_button = tkinter.Button(text='Upload', bg='#DA902D', width=7,
                                font=("Times%New%Roman", 15, "bold"),
                                relief="solid", command=upload)
-upload_button.place(relx=0.6, rely=0.8, anchor=CENTER)
+upload_button.place(relx=0.6, rely=0.6, anchor=CENTER)
 
 window.mainloop()
